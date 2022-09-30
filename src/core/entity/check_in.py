eac71077ff740f4f523251cf36ca5c9b761f6876
Fylype Wase <fylype.lira@boavistascpc.com.br>
2022-09-30 16:18:38 -0400

    add __eq__ method

--- src/core/entity/check_in.py
+++ src/core/entity/check_in.py
@@ -2,20 +2,22 @@ from datetime import datetime
 
 
 class CheckIn:
-    def __init__(self,
-    id: str,
-    person_id: str,
-    date_time: datetime
-    ) -> None:
+    def __init__(self, id: str, person_id: str, date_time: datetime) -> None:
         self.id = id
         self.person_id = person_id
         self.date_time = date_time
 
-    def get_id(self):
-        return self.id
-
-    def get_person_id(self):
+    def get_person_id(self) -> str:
         return self.person_id
 
-    def get_date_time(self):
-        return self.date_time
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, self.__class__):
+            return all(
+                [
+                    self.id == other.id,
+                    self.person_id == other.person_id,
+                    self.date_time == other.date_time,
+                ]
+            )
+
+        return False
