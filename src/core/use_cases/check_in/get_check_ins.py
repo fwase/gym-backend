d38ce290da84f42ec433dadc7e787fcce95e77a4
Fylype Wase <fylype.lira@boavistascpc.com.br>
2022-09-30 16:19:46 -0400

    update use_cases

--- src/core/use_cases/check_in/get_check_ins.py
+++ src/core/use_cases/check_in/get_check_ins.py
@@ -1,14 +1,13 @@
 from datetime import datetime
+
 from src.core.entity import CheckIn
+from src.core.repository.check_in_repository import CheckInRepository
+
 
 class GetCheckIns:
-    def __init__(
-        self,
-        repository: str,
-    ) -> None:
-        self.repository = repository
+    def __init__(self, check_in_repository: CheckInRepository, person_id: str) -> None:
+        self.check_in_repository = check_in_repository
+        self.person_id = person_id
 
-    def execute(self):
-        check_ins = self.repository
-
-        return check_ins
+    def execute(self) -> list[CheckIn]:
+        return self.check_in_repository.get_check_ins(self.person_id)
--- src/core/use_cases/check_in/make_check_in.py
+++ src/core/use_cases/check_in/make_check_in.py
@@ -1,20 +1,21 @@
-from src.core.entity import CheckIn
 from datetime import datetime
+
+from src.core.entity import CheckIn
+from src.core.repository.check_in_repository import CheckInRepository
+from src.infra.repository.check_in_repository_memory import CheckInRepositoryMemory
+
 
 class MakeCheckIn:
     def __init__(
-        self,
-        repository: str,
-        person_id: str
+        self, check_in_repository: CheckInRepositoryMemory, person_id: str
     ) -> None:
-        datetime_now = datetime.now()
-        id = self.get_timestamp(datetime_now)
+        self.check_in_repository = check_in_repository
+        self.person_id = person_id
 
-        self.repository = repository
-        self.check_in = CheckIn(id, person_id, datetime_now)
-
-    def get_timestamp(datetime):
+    def __get_timestamp():
         return int(datetime.now().timestamp() * 1000)
 
-    def execute(self) -> bool:
-        return self.repository
+    def execute(self) -> CheckIn:
+        check_in = CheckIn(1, self.person_id, datetime.now())
+
+        return self.check_in_repository.make_check_in(check_in)
