de14236d2b60ceac1cb29f16230d730a656e3fe2
Fylype Wase <fylypexp@gmail.com>
2023-10-02 22:21:35 -0400

    add cpf digits validation

--- src/api/routers/person.py
+++ src/api/routers/person.py
@@ -1,14 +1,20 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 
 from src.resources.impl.person_impl import PersonResourceImpl as person_resource
 
 from ..schemas.person import GetPersonSchema, PostPersonSchema
+
+import re
 
 router = APIRouter()
 
 
 @router.get("/persons", status_code=status.HTTP_200_OK, response_model=GetPersonSchema)
 def get_person(cpf: str) -> GetPersonSchema:
+    cpf_match = re.search(r"\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$", cpf)
+    if not cpf_match:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
     return person_resource.get_person(cpf)
 
 
